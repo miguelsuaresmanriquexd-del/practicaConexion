9f4f1e0e1affa919f48debac37f7afbbcc098df2
Aprendiz <manuvalenciamahecha13@gmail.com>
2026-08-31 17:14:14 -0500

    se creo una funcion index y se listo las personas

--- app/models/Persona.php
+++ app/models/Persona.php
@@ -3,16 +3,16 @@
 require_once __DIR__ . '/../../config/Conexion.php';
 
 class Persona {
-    private $conexion;
-    private $tabla = "persona";
+    
+    public $conexion;
+    public $tabla = "persona";
+
+    public $id;
+    public $nombre;
+    public $edad;
+    public $correo;
 
     
-    private $id;
-    private $nombre;
-    private $edad;
-    private $correo;
-
-    // Método Constructor
     public function __construct($id = null, $nombre = null, $edad = null, $correo = null) {
         $database = new Conexion();
         $this->conexion = $database->conectar();
@@ -22,19 +22,6 @@ class Persona {
         $this->edad = $edad;
         $this->correo = $correo;
     }
-
-    
-    public function getId() { return $this->id; }
-    public function setId($id) { $this->id = $id; }
-
-    public function getNombre() { return $this->nombre; }
-    public function setNombre($nombre) { $this->nombre = $nombre; }
-
-    public function getEdad() { return $this->edad; }
-    public function setEdad($edad) { $this->edad = $edad; }
-
-    public function getCorreo() { return $this->correo; }
-    public function setCorreo($correo) { $this->correo = $correo; }
 
     
     public function listaPersonas() {
